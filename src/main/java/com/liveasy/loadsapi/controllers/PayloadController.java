8fad983ed18df45fdab1029565258afa0f078c26
Anush Saxena <saxenaanush88@gmail.com>
2023-07-19 18:55:33 +0530

    via Terminal

--- src/main/java/com/liveasy/loadsapi/controllers/PayloadController.java
+++ src/main/java/com/liveasy/loadsapi/controllers/PayloadController.java
@@ -2,6 +2,7 @@ package com.liveasy.loadsapi.controllers;
 
 import com.liveasy.loadsapi.entity.PayloadEntity;
 import com.liveasy.loadsapi.service.PayloadService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.HttpClientErrorException;
 
 
 @RestController
@@ -17,9 +19,19 @@ public class PayloadController {
     @Autowired
     PayloadService payloadService;
 
+    public boolean valid(PayloadEntity validateEntity){
+        return !(validateEntity.getShipperId() == null || validateEntity.getDate() == null || validateEntity.getLoadingPoint() == null ||
+                validateEntity.getProductType() == null || validateEntity.getTruckType() == null || validateEntity.getUnloadingPoint() == null
+                || validateEntity.getNoOfTrucks() == null || validateEntity.getWeight() == null);
+    }
+
     @RequestMapping(method = RequestMethod.POST, value = "/load")
-    public String addLoadDetails(@RequestBody PayloadEntity payloadEntity){
-        return payloadService.addLoadDetails(payloadEntity);
+    public PayloadEntity addLoadDetails(@RequestBody PayloadEntity payloadEntity){
+        if (valid(payloadEntity)){
+            return payloadService.addLoadDetails(payloadEntity);
+        }
+        throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, " Some details are left empty");
+
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/load")
@@ -32,8 +44,11 @@ public class PayloadController {
         return payloadService.getLoadDetails(shipperId);
     }
     @RequestMapping(method = RequestMethod.PUT, value = "/load/{loadId}")
-    public String putLoadDetails(@PathVariable("loadId") String shipperId, @RequestBody PayloadEntity requestPayload){
-        return payloadService.putLoadDetails(shipperId, requestPayload);
+    public PayloadEntity putLoadDetails(@PathVariable("loadId") String shipperId, @RequestBody PayloadEntity requestPayload){
+        if (valid(requestPayload)){
+            return payloadService.putLoadDetails(shipperId, requestPayload);
+        }
+        throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, " Some details are left empty");
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/load/{loadId}")
--- src/main/java/com/liveasy/loadsapi/service/PayloadService.java
+++ src/main/java/com/liveasy/loadsapi/service/PayloadService.java
@@ -2,9 +2,12 @@ package com.liveasy.loadsapi.service;
 
 import com.liveasy.loadsapi.entity.PayloadEntity;
 import com.liveasy.loadsapi.repositories.PayloadRepo;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.HttpClientErrorException;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
@@ -12,41 +15,38 @@ public class PayloadService {
     @Autowired
     private PayloadRepo payloadRepo;
 
-    public String addLoadDetails(PayloadEntity payloadEntity){
-        if (!payloadRepo.existsById(payloadEntity.getShipperId())){
-            payloadRepo.save(payloadEntity);
-            return "Loads details added Successfully";
-        }
-        else{
-            return "Loads detail exist with this Shipper ID";
-        }
-    }
-    public PayloadEntity getLoadDetails(String shipperId){
-        if (payloadRepo.existsById(shipperId)){
-            return payloadRepo.findById(shipperId).get();
-        }
-        else{
-            return null;
-        }
-    }
-    public String putLoadDetails(String shipperId, PayloadEntity requestPayload){
-        if(payloadRepo.existsById(shipperId)){
-            requestPayload.setShipperId(shipperId);
+    public PayloadEntity addLoadDetails(PayloadEntity requestPayload){
+        Optional<PayloadEntity> payloadEntity = payloadRepo.findById(requestPayload.getShipperId());
+        if (payloadEntity.isEmpty()) {
             payloadRepo.save(requestPayload);
-            return "Details Updated";
+            return requestPayload;
         }
-        else{
-            return "Shipper Id does not exists!";
-        }
-    }
-    public String deleteLoadDetails( String shipperId){
-        if (payloadRepo.existsById(shipperId)){
-            payloadRepo.deleteById(shipperId);
-            return "Pay Load Details deleted successfully";
-        }
-        else{
-            return "Shipper Id doesn't exists";
-        }
+        throw new HttpClientErrorException(HttpStatus.BAD_REQUEST," shipperId already Exists! ");
     }
 
+    public PayloadEntity getLoadDetails(String shipperId){
+        Optional<PayloadEntity> responseEntity = payloadRepo.findById(shipperId);
+        if (responseEntity.isPresent()){
+            return responseEntity.get();
+        }
+        throw new HttpClientErrorException(HttpStatus.BAD_REQUEST," shipperId doesn't Exists! ");
+    }
+    public PayloadEntity putLoadDetails(String shipperId, PayloadEntity requestPayload){
+        Optional<PayloadEntity> payloadEntity = payloadRepo.findById(shipperId);
+        if (payloadEntity.isPresent()){
+            requestPayload.setShipperId(shipperId);
+            payloadRepo.save(requestPayload);
+            return requestPayload;
+        }
+        throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "shipperId doesn't exists");
+    }
+    public String deleteLoadDetails( String shipperId){
+        try {
+            payloadRepo.deleteById(shipperId);
+            return " Shipping Details Deleted ";
+        }
+        catch (Exception e){
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "shipperId doesn't exists");
+        }
+    }
 }
